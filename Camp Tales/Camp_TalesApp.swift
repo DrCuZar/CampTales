3300ab6ceab24695b8d2a8dc724c16e7ada7d744
Cannon Goldsby <cannon.goldsby2002@gmail.com>
2024-05-16 08:59:52 -0400

    Initial Commit

---
+++ Camp Tales/Camp_TalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camp_TalesApp.swift
+//  Camp Tales
+//
+//  Created by Cannon Goldsby on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Camp_TalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
